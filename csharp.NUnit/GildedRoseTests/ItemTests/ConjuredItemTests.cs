224714d3f14948d7fe59d98cc0bd10909dc58cc9
Benjamin Zelenay <if23b198@technikum-wien.at>
2025-06-10 22:28:59 +0200

    added conjured item tests
    all of them failed because the implementation is non existent
    
    ConjuredItem_DegradesTwiceAsFast_BeforeSellDate()
    ConjuredItem_DegradesFour_AfterSellDate()
     ConjuredItem_QualityNeverGoesBelowZero()

--- csharp.NUnit/GildedRoseTests/ItemTests/ConjuredItemTests.cs
+++ csharp.NUnit/GildedRoseTests/ItemTests/ConjuredItemTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GildedRoseKata;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace GildedRoseTests
 {
-    internal class ConjuredItemTests
+    [TestFixture]
+    public class ConjuredItemTests
     {
+        [Test]
+        public void ConjuredItem_DegradesTwiceAsFast_BeforeSellDate()
+        {
+            var item = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 10 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(2));
+            Assert.That(item.Quality, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void ConjuredItem_DegradesFour_AfterSellDate()
+        {
+            var item = new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 10 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(-1));
+            Assert.That(item.Quality, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void ConjuredItem_QualityNeverGoesBelowZero()
+        {
+            var item = new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 3 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(-1));
+            Assert.That(item.Quality, Is.EqualTo(0));
+        }
     }
 }
