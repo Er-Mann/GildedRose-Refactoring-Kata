be5ce24ab32599a19f141e9115bb1d48f8b0ffd7
Benjamin Zelenay <if23b198@technikum-wien.at>
2025-06-10 21:19:20 +0200

    addes Sulfuras Tests
    Sulfuras_RemainsConstant_EvenAfterExpiration()
    Sulfuras_QualityAndSellIn_DoNotChange()

--- csharp.NUnit/GildedRoseTests/ItemTests/SulfurasTests.cs
+++ csharp.NUnit/GildedRoseTests/ItemTests/SulfurasTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GildedRoseKata;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace GildedRoseTests
 {
-    internal class SulfurasTests
+    [TestFixture]
+    public class SulfurasTests      // Quality always stays 80 , sellIn does not change
     {
+        [Test]
+        public void Sulfuras_QualityAndSellIn_DoNotChange()
+        {
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 80 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(5));
+            Assert.That(item.Quality, Is.EqualTo(80));
+        }
+
+        [Test]
+        public void Sulfuras_RemainsConstant_EvenAfterExpiration()
+        {
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(-1));
+            Assert.That(item.Quality, Is.EqualTo(80));
+        }
     }
 }
