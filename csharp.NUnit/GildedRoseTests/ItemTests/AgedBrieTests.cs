d1787dc7ea33cdd8797d7931990b253e5eb1cb65
Benjamin Zelenay <if23b198@technikum-wien.at>
2025-06-10 21:03:05 +0200

    added aged brie tests
    AgedBrie_IncreasesInQuality_AsItGetsOlder()
    AgedBrie_QualityIncreasesTwice_AsSellInExpires()
    AgedBrie_QualityDoesNotExceedFifty()

--- csharp.NUnit/GildedRoseTests/ItemTests/AgedBrieTests.cs
+++ csharp.NUnit/GildedRoseTests/ItemTests/AgedBrieTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GildedRoseKata;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace GildedRoseTests
 {
-    internal class AgedBrieTests
+    [TestFixture]
+    public class AgedBrieTests
     {
+        [Test]
+        public void AgedBrie_IncreasesInQuality_AsItGetsOlder()
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = 10, Quality = 25 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(9));
+            Assert.That(item.Quality, Is.EqualTo(26)); // normal incease by 1
+        }
+
+        [Test]
+        public void AgedBrie_QualityIncreasesTwice_AsSellInExpires()
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = 0, Quality = 25 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(-1));
+            Assert.That(item.Quality, Is.EqualTo(27)); // +2 after expiration
+        }
+
+        [Test]
+        public void AgedBrie_QualityDoesNotExceedFifty()
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(1));
+            Assert.That(item.Quality, Is.EqualTo(50)); // still capped
+        }
     }
 }
