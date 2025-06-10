ef03e52b956864d7101eaeec518776563e62eb38
Benjamin Zelenay <if23b198@technikum-wien.at>
2025-06-10 20:45:06 +0200

    added files for each item type
    Types[Normal, Brie, Sulfuras, Backstage, Conjured]

---
+++ csharp.NUnit/GildedRoseTests/ItemTests/NormalItemTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using GildedRoseKata;
+using System.Collections.Generic;
+
+namespace GildedRoseTests
+{
+    [TestFixture]
+    public class NormalItemTests
+    {
+        [Test]
+        public void NormalItem_Degrades_By1_BeforeSellDate()    // normal item, standard behavior when sell date is not passed
+        {
+            var item = new Item { Name = "Basic Item", SellIn = 5, Quality = 10 };
+            var app = new GildedRose(new List<Item> { item });
+
+            app.UpdateQuality();
+
+            Assert.That(item.SellIn, Is.EqualTo(4));
+            Assert.That(item.Quality, Is.EqualTo(9));
+        }
+
+    }
+}
